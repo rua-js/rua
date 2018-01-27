5dd60e65014231a0163c33c312f0291499f1f011
copydog <775150@qq.com>
2018-01-27 20:19:35 +0800

    ready for 0.0.1

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import { storage } from 'rua-storage/lib';
+import { event } from 'rua-event/lib';
+import { dvaLite, ruaDva, actions } from 'rua-dva';
+export { storage, event, dvaLite, ruaDva, actions };
