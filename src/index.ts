0981bc4e0870a6dbfdb725c2a44e20a4a99599f1
hsy128 <8775219@qq.com>
2018-01-27 20:15:13 +0800

    exported 3 packages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+// Foundation
+import { storage } from 'rua-storage/lib'
+import { event } from 'rua-event/lib'
 
+// Basic
+import { dvaLite, ruaDva, actions } from 'rua-dva'
+
+// Advanced
+
+// React / React Native Only
 
 
 export {
-
+  // rua-storage
+  storage,
+  // rua-event
+  event,
+  // rua-dva
+  dvaLite,
+  actions
 }
