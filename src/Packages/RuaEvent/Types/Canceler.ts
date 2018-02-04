82aa466817c2b9633ae6001cca7a225674032d4b
copydog <775150@qq.com>
2018-02-04 15:53:35 +0800

    integrate RuaEvent

---
+++ src/Packages/RuaEvent/Types/Canceler.ts
@@ -0,0 +1,2 @@
+type Canceler = Function
+export default Canceler
---
+++ src/Packages/RuaEvent/Types/index.ts
@@ -0,0 +1,7 @@
+import Canceler from './Canceler'
+import MultiEvents from './MultiEvents'
+
+export {
+  Canceler,
+  MultiEvents,
+}
