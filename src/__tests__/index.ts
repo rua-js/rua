c4e1a6573842a219500fcf4f2a34fd9fca1ff341
copydog <775150@qq.com>
2018-02-04 19:32:29 +0800

    tests

---
+++ src/__tests__/index.ts
@@ -0,0 +1,26 @@
+import {
+  storage,
+  event,
+  Exception,
+} from '../index'
+
+import RuaStorage from '../packages/RuaStorage/RuaStorage'
+import RuaEvent from '../packages/RuaEvent/RuaEvent'
+
+describe('Rua Index Test', () => {
+  test('exportation', () => {
+    // *** Primitives ***
+    // case: storage
+    expect(
+      storage instanceof RuaStorage
+    ).toBeTruthy()
+    // case: event
+    expect(
+      event instanceof RuaEvent
+    ).toBeTruthy()
+    // case: Exception
+    expect(
+      new Exception() instanceof Error
+    ).toBeTruthy()
+  })
+})
