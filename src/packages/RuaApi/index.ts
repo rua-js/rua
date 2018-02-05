aace3e466ab0c4c868f33dcc7bf29e8d8c0bc0fb
copydog <775150@qq.com>
2018-02-06 00:19:15 +0800

    x RuaApi export

--- src/packages/RuaApi/index.ts
+++ src/packages/RuaApi/index.ts
@@ -2,6 +2,8 @@ import RuaApiClass from './RuaApi'
 import { packager } from 'rua-core/lib'
 import { RuaApiInterface } from './Interface'
 
+const instance = <RuaApiClass>packager.registerIfNotRegistered('rua-api', new RuaApiClass())
+
 export const RuaApi = RuaApiClass
 
-export const api = <RuaApiClass>packager.registerIfNotRegistered('rua-api', new RuaApiClass())
+export const api = instance.dispatch
