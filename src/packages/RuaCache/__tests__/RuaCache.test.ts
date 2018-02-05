0bb598e75a76a2c4b211be2e400172280d946100
copydog <775150@qq.com>
2018-02-05 15:55:54 +0800

    test for RuaCache

--- src/packages/RuaCache/__tests__/RuaCache.test.ts
+++ src/packages/RuaCache/__tests__/RuaCache.test.ts
@@ -24,4 +24,60 @@ describe('RuaCache Tests(cache part)', () => {
       cache.get('test1')
     ).toBe(undefined)
   })
+
+  test('.all', () => {
+    // prep: set data
+    cache.set('test1', 'test-here1')
+    cache.set('test2', 'test-here2')
+    // case: .all
+    expect(
+      cache.all()
+    ).toEqual({
+      test1: 'test-here1',
+      test2: 'test-here2',
+    })
+  })
+
+  test('.clear', () => {
+    // prep: set data
+    cache.set('test1', 'test-here1')
+    cache.set('test2', 'test-here2')
+    // case: .clear
+    expect(
+      cache.clear()
+    ).toEqual({
+      test1: 'test-here1',
+      test2: 'test-here2',
+    })
+    // case: check removal
+    expect(
+      cache.all()
+    ).toEqual({})
+  })
+
+  test('.length', () => {
+    // prep: set data
+    cache.set('test1', 'test-here1')
+    cache.set('test2', 'test-here2')
+    // case: .length
+    expect(
+      cache.length()
+    ).toBe(2)
+    // prep: removal
+    cache.remove('test2')
+    // case: .length
+    expect(
+      cache.length()
+    ).toBe(1)
+  })
+
+  test('.keys', () => {
+    // prep: set data
+    cache.set('test1', 'test-here1')
+    cache.set('test2', 'test-here2')
+    // case: .keys
+    expect(
+      cache.keys()
+    ).toEqual(['test1', 'test2'])
+  })
 })
