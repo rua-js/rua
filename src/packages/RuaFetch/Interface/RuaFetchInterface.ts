5d81738c330c34de46a0b2be29899ee7b038c0ea
copydog <775150@qq.com>
2018-02-06 17:32:17 +0800

    up RuaFetchInterface

--- src/packages/RuaFetch/Interface/RuaFetchInterface.ts
+++ src/packages/RuaFetch/Interface/RuaFetchInterface.ts
@@ -1,6 +1,33 @@
+import AnyObject from 'rua-core/lib/Types/AnyObject'
 
 interface RuaFetchInterface {
+  /**
+   * Interceptors
+   *
+   * @type {Object}
+   */
+  interceptor: AnyObject
 
+  /**
+   * Url
+   *
+   * @type {String}
+   */
+  url: string
+
+  /**
+   * Options
+   *
+   * @type {Object}
+   */
+  options: AnyObject
+
+  /**
+   * Starts the request
+   *
+   * @returns {Promise<Response>}
+   */
+  start(): Promise<Response>
 }
 
 export default RuaFetchInterface
--- src/packages/RuaFetch/Interface/index.ts
+++ src/packages/RuaFetch/Interface/index.ts
@@ -1,7 +1,9 @@
 import RuaFetchStatic from './RuaFetchStatic'
 import InterceptorInterface from './InterceptorInterface'
+import RuaFetchInterface from './RuaFetchInterface'
 
 export {
   RuaFetchStatic,
   InterceptorInterface,
+  RuaFetchInterface,
 }
