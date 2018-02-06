023d51f46fa7c56273e3412e1ebf5f363c70ce45
copydog <775150@qq.com>
2018-02-06 17:12:33 +0800

    + interceptor interface

---
+++ src/packages/RuaFetch/Interface/InterceptorInterface.ts
@@ -0,0 +1,32 @@
+import AnyObject from 'rua-core/lib/Types/AnyObject'
+
+interface InterceptorInterface {
+  /**
+   * Keeps all interceptors
+   */
+  interceptor: AnyObject
+
+  /**
+   * Adds one interceptor with name
+   *
+   * @param {string} name
+   * @param {Function} interceptor
+   */
+  add(name: string, interceptor: Function): void
+
+  /**
+   * Removes one interceptor with name
+   *
+   * @param {string} name
+   */
+  remove(name: string): void
+
+  /**
+   * Gets all interceptors
+   *
+   * @returns {Function[]}
+   */
+  all(): AnyObject
+}
+
+export default InterceptorInterface
--- src/packages/RuaFetch/Interface/index.ts
+++ src/packages/RuaFetch/Interface/index.ts
@@ -1,5 +1,7 @@
 import RuaFetchStatic from './RuaFetchStatic'
+import InterceptorInterface from './InterceptorInterface'
 
 export {
   RuaFetchStatic,
+  InterceptorInterface,
 }
