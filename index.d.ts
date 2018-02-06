512747861b1232c0d847bb053e03ceff1cdd1af7
hsy128 <8775219@qq.com>
2018-02-06 14:12:50 +0800

    hit 0.1.1

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,6 @@ import { event } from './lib/packages/RuaEvent';
 import { Exception } from './lib/packages/RuaException';
 import { fetch, catchedFetch, rawFetch } from './lib/packages/RuaFetch';
 import { cache } from './lib/packages/RuaCache';
+import { api } from './lib/packages/RuaApi';
 import { dvaLite, ruaDva, actions } from 'rua-dva';
-export { dvaLite, ruaDva, actions, event, Exception, storage, fetch, catchedFetch, rawFetch, cache };
+export { dvaLite, ruaDva, actions, event, Exception, storage, fetch, catchedFetch, rawFetch, cache, api };
