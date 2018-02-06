bc5fea0a1ac2291df97473fdf6a7a488ea596171
copydog <775150@qq.com>
2018-02-06 14:01:08 +0800

    finish tests for RuaApi

--- src/packages/RuaApi/__tests__/RuaApi.test.ts
+++ src/packages/RuaApi/__tests__/RuaApi.test.ts
@@ -1,4 +1,5 @@
 import RuaApi from '../RuaApi'
+import { api } from '../index'
 
 describe('RuaApi Tests', () => {
   test('init', () => {
@@ -6,5 +7,24 @@ describe('RuaApi Tests', () => {
     expect(
       new RuaApi() instanceof RuaApi
     ).toBeTruthy()
+  })
+  test('usage', async () => {
+    // prep: load api
+    api.load({
+      test: {
+        go: {
+          url: 'https://reqres.in/api/users',
+          method: 'GET',
+        }
+      }
+    })
+    // case: success
+    await expect(
+      api.call('test.go')
+    ).resolves.toBeInstanceOf(Object)
+    // case: correct data
+    await expect(
+      api.call('test.go')
+    ).resolves.toHaveProperty('page')
   })
 })
