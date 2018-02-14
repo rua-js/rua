d31f00897ce374a33fdfcbc96fdc2c638ce308a5
copydog <775150@qq.com>
2018-02-14 18:37:56 +0800

    clean up Rua

--- src/packages/Rua.ts
+++ src/packages/Rua.ts
@@ -1,26 +1,4 @@
-import AbstractRuaPackage from 'rua-core/lib/Abstractions/AbstractRuaPackage'
-import { NilableObject, AnyObject } from 'rua-core/lib/Types'
 
-class Rua extends AbstractRuaPackage {
+class Rua {
 
-  /**
-   * Default configuration
-   *
-   * @type {AnyObject}
-   */
-  protected configuration: AnyObject = {}
-
-  /**
-   * Configures Rua.js packages
-   *
-   * @param {NilableObject} config
-   * @returns {void | AnyObject}
-   */
-  public config(config?: NilableObject): void | AnyObject {
-    if (!config) {
-      return this.configuration
-    }
-
-    this.configuration = {...this.configuration, ...config}
-  }
 }
