42876288470c8d0863344831727edbafed111d50
copydog <775150@qq.com>
2018-02-17 18:17:29 +0800

    fix ts

--- src/packages/Localization/index.ts
+++ src/packages/Localization/index.ts
@@ -2,7 +2,7 @@ import Localization from './Localization'
 
 import { packager } from 'rua-core/lib'
 
-const locale: Localization = packager.registerIfNotRegistered('rua-localization',  new Localization())
+const locale: Localization = <Localization>packager.registerIfNotRegistered('rua-localization',  new Localization())
 
 const __: Function = locale.translate
 
