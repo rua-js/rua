9539519e60d11cceabc841542917512055f2682f
copydog <775150@qq.com>
2018-02-22 17:55:11 +0800

    working on global configuration

--- src/packages/Rua/Contract/RuaConfiguration.ts
+++ src/packages/Rua/Contract/RuaConfiguration.ts
@@ -1,4 +1,13 @@
+
 interface RuaConfiguration {
+  fetch?: {
+    headers?: object
+    extraHeaders?: object
+    requestInterceptors?: Function[]
+    responseInterceptors?: Function[]
+    driver?: Function
+  }
+
   [key: string]: any
 }
 
--- src/packages/Rua/Rua.ts
+++ src/packages/Rua/Rua.ts
@@ -1,19 +1,34 @@
 import { CanConfig } from 'rua-core/lib/Contracts'
 import { AnyObject } from 'rua-core/lib/Types'
+import { AbstractRuaPackage } from 'rua-core/lib/Abstractions'
+
+import { RuaConfiguration } from './Contract'
 
 /**
  * Rua Class manages/configs all packages
  *
  * @class Rua.js
  */
-class Rua implements CanConfig
+class Rua extends AbstractRuaPackage implements CanConfig
 {
 
-  public configuration = {
+  /**
+   * Default configuration
+   *
+   * @type {object}
+   */
+  public defaultConfiguration: RuaConfiguration = {}
 
-  }
+  /**
+   * Modules that can config
+   *
+   * @type {string[]}
+   */
+  public configurableModules: string[] = [
+    'fetch',
+  ]
 
-  public config(configuration?: AnyObject): Rua
+  public config(configuration?: RuaConfiguration): Rua
   {
     return this
   }
