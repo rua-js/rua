238c1c2d9997aafa11d703cc4840f302ee3a8aeb
copydog <775150@qq.com>
2018-02-23 13:37:54 +0800

    [E] op tests

--- src/packages/Exception/__tests__/Exception.test.ts
+++ src/packages/Exception/__tests__/Exception.test.ts
@@ -12,12 +12,30 @@ describe('Exception', () => {
       e instanceof Error
     ).toBeTruthy()
   })
+
   test('property', () => {
     const e = new Exception('property')
     // case: name
     expect(
       e.name
     ).toBe('Exception')
+    // case: message
+    expect(
+      e.message
+    ).toBe('property')
+    // case: stack
+    expect(
+      e.stack
+    ).toBeTruthy()
+  })
+
+  test('inherit', () => {
+    class InheritorException extends Exception {}
+    const e = new InheritorException('property')
+    // case: name
+    expect(
+      e.name
+    ).toBe('InheritorException')
     // case: message
     expect(
       e.message
