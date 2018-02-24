7b4f74f3fcf6b48f3840f9b00eec9a444cc91208
copydog <775150@qq.com>
2018-02-24 23:23:23 +0800

    drafting Is

--- src/packages/Is/Interface/IsInterface.ts
+++ src/packages/Is/Interface/IsInterface.ts
@@ -2,6 +2,10 @@
 interface IsInterface {
   email(email: string): boolean
   phone(phone: string | number): boolean
+  string(string: any): boolean
+  number(number: any): boolean
+  numeric(number: any): boolean
+  float(number: any): boolean
 }
 
 export default IsInterface
