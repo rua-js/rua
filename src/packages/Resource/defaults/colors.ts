bac812b390ab299efb434251b1f6ff7c38c49df3
copydog <775150@qq.com>
2018-05-12 12:07:13 +0800

    add color schema

---
+++ src/packages/Resource/defaults/colors.ts
@@ -0,0 +1,26 @@
+const colors = {
+  background: '#f2f2f2',
+  primary: '#ed393a',
+  secondary: '#fd7722',
+  white: '#fff',
+  black: '#1b1c1d',
+  red: '#db2828',
+  orange: '#f2711c',
+  yellow: '#fbbd08',
+  olive: '#b5cc18',
+  green: '#21ba45',
+  teal: '#00b5ad',
+  blue: '#2185d0',
+  violet: '#6435c9',
+  purple: '#a333c8',
+  pink: '#e03997',
+  brown: '#a5673f',
+  grey: '#767676',
+  borderLight: '#f0f0f0',
+  border: '#ddd',
+  // social media
+  wechat: '#62b900',
+  eleme: '#1e89e0',
+}
+
+export default colors
