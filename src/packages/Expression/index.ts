6f65c9a990d6ab2db790159342482120338464e5
copydog <775150@qq.com>
2018-06-10 15:08:58 +0800

    add Expression

---
+++ src/packages/Expression/index.ts
@@ -0,0 +1,43 @@
+import Patterns from './Patterns'
+
+/** Patterns */
+const {
+  Email,
+  QQEmail,
+  Domain,
+  Chinese,
+  HexColor,
+  CreditCard,
+  VISACreditCard,
+  MASTERCARDCreditCard,
+  MD5,
+  SHA1,
+  SHA256,
+  ISBN13,
+  ISBN10,
+  MACAddress,
+  SSN,
+  IPv4,
+  IPv6Pattern,
+} = Patterns
+
+
+export {
+  Email,
+  QQEmail,
+  Domain,
+  Chinese,
+  HexColor,
+  CreditCard,
+  VISACreditCard,
+  MASTERCARDCreditCard,
+  MD5,
+  SHA1,
+  SHA256,
+  ISBN13,
+  ISBN10,
+  MACAddress,
+  SSN,
+  IPv4,
+  IPv6Pattern,
+}
