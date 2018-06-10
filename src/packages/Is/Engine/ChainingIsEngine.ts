7bae017d8fce8d79b8a773418dab973bee8940c2
copydog <775150@qq.com>
2018-06-10 15:44:03 +0800

    reorder ChainingIsEngine

--- src/packages/Is/Engine/ChainingIsEngine.ts
+++ src/packages/Is/Engine/ChainingIsEngine.ts
@@ -1,10 +1,11 @@
 import * as _ from 'lodash'
 
-class Is
+class ChainingIsEngine
 {
   public result: any = null
   protected data: any = null
   protected orLogic: boolean = false
+  protected xorLogic: boolean = false
   protected notLogic: boolean = false
 
   constructor(data: any)
@@ -16,21 +17,27 @@ class Is
   /**
    * Logic comparator
    *
-   * @returns {Is}
+   * @returns {ChainingIsEngine}
    */
-  public get and(): Is
+  public get and(): ChainingIsEngine
   {
     this.orLogic = false
     return this
   }
 
-  public get or(): Is
+  public get or(): ChainingIsEngine
   {
     this.orLogic = true
     return this
   }
 
-  public get not(): Is
+  // public get xor(): ChainingIsEngine
+  // {
+  //   this.xorLogic = true
+  //   return this
+  // }
+
+  public get not(): ChainingIsEngine
   {
     this.notLogic = true
     return this
@@ -39,274 +46,277 @@ class Is
   /**
    * Conditions
    *
-   * @returns {Is}
+   * @returns {ChainingIsEngine}
    */
-  public get string(): Is
+
+  // ======= None-lodash methods which requires test
+  public get exception(): ChainingIsEngine
+  {
+    console.error('ChainingIsEngine.exception is NOT implemented yet')
+    return this
+  }
+
+  public get false(): ChainingIsEngine
+  {
+    return this.runLogic(
+      false
+    )
+  }
+
+  public get true(): ChainingIsEngine
+  {
+    return this.runLogic(
+      true
+    )
+  }
+
+  // ======= Lodash methods which do NOT need test
+  public get string(): ChainingIsEngine
   {
     return this.runLogic(
       _.isString(this.data)
     )
   }
 
-  public get array(): Is
+  public get array(): ChainingIsEngine
   {
     return this.runLogic(
       _.isArray(this.data)
     )
   }
 
-  public get arrayBuffer(): Is
+  public get arrayBuffer(): ChainingIsEngine
   {
     return this.runLogic(
       _.isArrayBuffer(this.data)
     )
   }
 
-  public get arrayLike(): Is
+  public get arrayLike(): ChainingIsEngine
   {
     return this.runLogic(
       _.isArrayLike(this.data)
     )
   }
 
-  public get arrayLikeObject(): Is
+  public get arrayLikeObject(): ChainingIsEngine
   {
     return this.runLogic(
       _.isArrayLikeObject(this.data)
     )
   }
 
-  public get boolean(): Is
+  public get boolean(): ChainingIsEngine
   {
     return this.runLogic(
       _.isBoolean(this.data)
     )
   }
 
-  public get buffer(): Is
+  public get buffer(): ChainingIsEngine
   {
     return this.runLogic(
       _.isBuffer(this.data)
     )
   }
 
-  public get date(): Is
+  public get date(): ChainingIsEngine
   {
     return this.runLogic(
       _.isDate(this.data)
     )
   }
 
-  public get element(): Is
+  public get element(): ChainingIsEngine
   {
     return this.runLogic(
       _.isElement(this.data)
     )
   }
 
-  public get empty(): Is
+  public get empty(): ChainingIsEngine
   {
     return this.runLogic(
       _.isEmpty(this.data)
     )
   }
 
-  public get error(): Is
+  public get error(): ChainingIsEngine
   {
     return this.runLogic(
       _.isError(this.data)
     )
   }
 
-  public get exception(): Is
-  {
-    console.error('Is.exception is NOT implemented yet')
-    return this
-  }
-
-  public get finite(): Is
+  public get finite(): ChainingIsEngine
   {
     return this.runLogic(
       _.isFinite(this.data)
     )
   }
 
-  public get function(): Is
+  public get function (): ChainingIsEngine
   {
     return this.runLogic(
       _.isFunction(this.data)
     )
   }
 
-  public get integer(): Is
+  public get integer(): ChainingIsEngine
   {
     return this.runLogic(
       _.isInteger(this.data)
     )
   }
 
-  public get length(): Is
+  public get length(): ChainingIsEngine
   {
     return this.runLogic(
       _.isLength(this.data)
     )
   }
 
-  public get Map(): Is
+  public get Map(): ChainingIsEngine
   {
     return this.runLogic(
       _.isMap(this.data)
     )
   }
 
-  public get NaN(): Is
+  public get NaN(): ChainingIsEngine
   {
     return this.runLogic(
       _.isNaN(this.data)
     )
   }
 
-  public get native(): Is
+  public get native(): ChainingIsEngine
   {
     return this.runLogic(
       _.isNative(this.data)
     )
   }
 
-  public get nil(): Is
+  public get nil(): ChainingIsEngine
   {
     return this.runLogic(
       _.isNil(this.data)
     )
   }
 
-  public get null(): Is
+  public get null(): ChainingIsEngine
   {
     return this.runLogic(
       _.isNull(this.data)
     )
   }
 
-  public get number(): Is
+  public get number(): ChainingIsEngine
   {
     return this.runLogic(
       _.isNumber(this.data)
     )
   }
 
-  public get object(): Is
+  public get object(): ChainingIsEngine
   {
     return this.runLogic(
       _.isObject(this.data)
     )
   }
 
-  public get objectLike(): Is
+  public get objectLike(): ChainingIsEngine
   {
     return this.runLogic(
       _.isObjectLike(this.data)
     )
   }
 
-  public get plainObject(): Is
+  public get plainObject(): ChainingIsEngine
   {
     return this.runLogic(
       _.isPlainObject(this.data)
     )
   }
 
-  public get RegExp(): Is
+  public get RegExp(): ChainingIsEngine
   {
     return this.runLogic(
       _.isRegExp(this.data)
     )
   }
 
-  public get safeInteger(): Is
+  public get safeInteger(): ChainingIsEngine
   {
     return this.runLogic(
       _.isSafeInteger(this.data)
     )
   }
 
-  public get Set(): Is
+  public get Set(): ChainingIsEngine
   {
     return this.runLogic(
       _.isSet(this.data)
     )
   }
 
-  public get symbol(): Is
+  public get symbol(): ChainingIsEngine
   {
     return this.runLogic(
       _.isSymbol(this.data)
     )
   }
 
-  public get TypedArray(): Is
+  public get TypedArray(): ChainingIsEngine
   {
     return this.runLogic(
       _.isTypedArray(this.data)
     )
   }
 
-  public get undefined(): Is
+  public get undefined(): ChainingIsEngine
   {
     return this.runLogic(
       _.isUndefined(this.data)
     )
   }
 
-  public get WeakMap(): Is
+  public get WeakMap(): ChainingIsEngine
   {
     return this.runLogic(
       _.isWeakMap(this.data)
     )
   }
 
-  public get WeakSet(): Is
+  public get WeakSet(): ChainingIsEngine
   {
     return this.runLogic(
       _.isWeakSet(this.data)
     )
   }
 
-  public get false(): Is
-  {
-    return this.runLogic(
-      false,
-    )
-  }
-
-  public get true(): Is
-  {
-    return this.runLogic(
-      true,
-    )
-  }
-
-  public match(other: any): Is
+  public match(other: any): ChainingIsEngine
   {
     return this.runLogic(
       _.isMatch(this.data, other)
     )
   }
 
-  public matchWith(other: any, customizer: _.isMatchCustomizer): Is
+  public matchWith(other: any, customizer: _.isMatchCustomizer): ChainingIsEngine
   {
     return this.runLogic(
       _.isMatchWith(this.data, other, customizer)
     )
   }
 
-  public equal(other: any): Is
+  public equal(other: any): ChainingIsEngine
   {
     return this.runLogic(
       _.isEqual(this.data, other)
     )
   }
 
-  public equalWith(other: any, customizer: _.IsEqualCustomizer): Is
+  public equalWith(other: any, customizer: _.IsEqualCustomizer): ChainingIsEngine
   {
     return this.runLogic(
       _.isEqualWith(this.data, other, customizer)
@@ -316,9 +326,9 @@ class Is
   /**
    *
    * @param {boolean} result
-   * @returns {Is}
+   * @returns {ChainingIsEngine}
    */
-  protected runLogic(result: boolean): Is
+  protected runLogic(result: boolean): ChainingIsEngine
   {
     let currentResult = result
 
@@ -340,6 +350,6 @@ class Is
   }
 }
 
-export default Is
+export default ChainingIsEngine
 
-// Is('string').string.and.boolean.or.lessThan
+// ChainingIsEngine('string').string.and.boolean.or.lessThan
