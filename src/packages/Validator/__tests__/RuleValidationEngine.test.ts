ed4f46ede4d3adf8a024572c457b629e1dafb139
copydog <775150@qq.com>
2018-06-11 20:42:26 +0800

    + more test on RuleValidationEngine

--- src/packages/Validator/__tests__/RuleValidationEngine.test.ts
+++ src/packages/Validator/__tests__/RuleValidationEngine.test.ts
@@ -5,7 +5,7 @@ describe('RuleValidationEngine', () =>
 {
   const rve = new RuleValidationEngine()
   rve.setValidators({
-    string: () => true,
+    string: str => typeof str === 'string',
     required: (data, forceTrue) => forceTrue || !!data,
   })
 
@@ -22,14 +22,30 @@ describe('RuleValidationEngine', () =>
 
   test('.validate', () =>
   {
-    // Regular Case
+    // Correct Case With One Rule
     const t1 = 111
     const rule1 = new Rules('required')
     expect(rve.validate(t1, rule1)).toBeTruthy()
 
+    // Correct Case With Two Rules
+    const t2 = '222'
+    const rule2 = new Rules('string|required')
+    expect(rve.validate(t2, rule2))
+
+    // Wrong Case With One Rule
+    const t3 = 333
+    const rule3 = new Rules('string')
+    expect(rve.validate(t3, rule3)).toBeFalsy()
+
+    // Wrong Case With Two Rule
+    const t4 = ''
+    const rule4 = new Rules('required|string')
+    expect(rve.validate(t4, rule4)).toBeFalsy()
+
     // With Params Case
-    const t2 = false
-    const rule2 = new Rules('required:true')
-    expect(rve.validate(t2, rule2)).toBeTruthy()
+    const t5 = false
+    const rule5 = new Rules('required:true')
+    expect(rve.validate(t5, rule5)).toBeTruthy()
+
   })
 })
