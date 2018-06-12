2ce2c25c7c076c59407f7b2ea7129d52cdd2c69e
copydog <775150@qq.com>
2018-06-12 22:35:06 +0800

    upgrade Connect

--- src/packages/Decorator/Connect.ts
+++ src/packages/Decorator/Connect.ts
@@ -4,12 +4,41 @@ import { connect } from 'react-redux'
 
 const Connect = (connectFunction: string[] | Function) =>
 {
-  if (_.isFunction(connectFunction)) {
+  if (_.isFunction(connectFunction))
+  {
     return connect(connectFunction as Function)
   }
 
-  return connect((store: any) => {
-    return _.pick(store, connectFunction as string[])
+  return connect((store: any) =>
+  {
+    const exportData: any = {}
+
+    for (const mapping of connectFunction as string[])
+    {
+      const [
+        pathOrName,
+        pathOptional,
+      ] = mapping.split(':')
+
+      // todo: last accessor of path can NOT be array index
+
+      let path: string
+      let exportName: string
+
+      if (pathOptional)
+      {
+        path = pathOptional
+        exportName = pathOrName
+      } else
+      {
+        path = pathOrName
+        exportName = pathOrName.split('.').pop()!
+      }
+
+      exportData[exportName] = _.get(store, path)
+    }
+
+    return
   })
 }
 
