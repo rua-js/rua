2a26df51a83fbcdc8c8a521e70e8edeb97349033
copydog <775150@qq.com>
2018-06-13 20:13:41 +0800

    + Immutable Decorator

--- src/packages/Decorator/index.ts
+++ src/packages/Decorator/index.ts
@@ -2,12 +2,14 @@ import Listen from './Listen'
 import Connect from './Connect'
 import Instance from './Instance'
 import API from './API'
+import AutoBind from './AutoBind'
 
 const Decorator = {
   Listen,
   Connect,
   Instance,
   API,
+  AutoBind,
 }
 
 export {
