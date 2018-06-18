182c33c4941efe7737fca2447aa3b7ee148fec97
copydog <775150@qq.com>
2018-06-18 14:42:14 +0800

    refactor(/index.ts): reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,25 @@
 // Master
-import { Rua } from './packages/rua'
+export { Rua } from './packages/rua'
 // Primitive library
-import { Storage } from './packages/storage'
-import { Event } from './packages/event'
-import { Exception } from './packages/exception'
-import { request } from './packages/request'
-import { convertor } from './packages/convertor'
-import { util } from './packages/utility'
-import { memory } from './packages/memory'
+export { Storage } from './packages/storage'
+export { Event } from './packages/event'
+export { Exception } from './packages/exception'
+export { Request } from './packages/request'
+export { convertor } from './packages/convertor'
+export { util } from './packages/utility'
+export { memory } from './packages/memory'
 // Essential library
-import { Cache } from './packages/cache'
-import { API } from './packages/api'
-import { __, locale, translate } from './packages/localization'
-import { Decorator, Decorator as D } from './packages/decorator'
-import { core, emptyArray, emptyObject, noop, dvaReducerGenerator } from './packages/shared'
+export { Cache } from './packages/cache'
+export { API } from './packages/api'
+export { __, locale, translate } from './packages/localization'
+export { Decorator, Decorator as D } from './packages/decorator'
+export { core, emptyArray, emptyObject, noop, dvaReducerGenerator } from './packages/shared'
 // Non-essential library
-import { actions, dvaLite } from './packages/dva'
-import { DataAdaptor } from './packages/data-adaptor'
+export { actions, dvaLite } from './packages/dva'
+export { DataAdaptor } from './packages/data-adaptor'
 // Third-party library
-import { _, connect } from './packages/third-party'
+export { _, connect } from './packages/third-party'
 
 // Deep-integrated library
 
 // React / React Native Only
-
-export {
-  // master
-  Rua,
-  // primitive
-  util,
-  convertor,
-  memory,
-  // rua-dva
-  dvaLite,
-  actions,
-  // rua-event
-  Event,
-  // rua-exception
-  Exception,
-  // rua-storage
-  Storage,
-  // rua-request
-  request,
-  // rua-cache
-  Cache,
-  // rua-api
-  API,
-  // rua-localization
-  locale,
-  translate,
-  __,
-  // rua-decorator
-  Decorator,
-  D,
-  // third-party
-  connect,
-  _,
-  // Constants
-  emptyArray,
-  emptyObject,
-  noop,
-  core,
-  // dva Reducers
-  dvaReducerGenerator,
-  // data-adaptor
-  DataAdaptor,
-}
