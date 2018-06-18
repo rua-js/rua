dfd0e3da079762cba19a91f8b7659ddc7b809e1a
copydog <775150@qq.com>
2018-06-19 01:41:25 +0800

    feat(data.ts): add FunctionObject

--- src/packages/type/data.ts
+++ src/packages/type/data.ts
@@ -1,12 +1,9 @@
 export type AnyData = any
-export interface AnyObject {
-  [key: string]: any
-}
+export interface AnyObject { [key: string]: any }
 export type AnyBoolean = boolean
 export type AnyArray = any[]
 
 export type Nilable = null | undefined
-
 export type NilableArray = any[] | undefined | null
 export type NilableBoolean = Nilable | AnyBoolean
 export type NilableObject = Nilable | AnyObject
@@ -17,3 +14,5 @@ export type NullableObject = AnyObject | Nullable
 export type Undefinable = undefined
 export type UndefinableBoolean = boolean | Undefinable
 export type UndefinableObject = AnyObject | Undefinable
+
+export type FunctionObject = { [key: string]: Function }
