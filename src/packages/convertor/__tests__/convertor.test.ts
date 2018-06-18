343c0c2c8847c2f7cf489c94f623c3c9a00644fa
copydog <775150@qq.com>
2018-06-18 14:37:49 +0800

    test(convertor.test.ts): tslint

---
+++ src/packages/convertor/__tests__/convertor.test.ts
@@ -0,0 +1,20 @@
+import Convertor from '../Convertor'
+
+describe('convertor Tests', () =>
+{
+  test('JSON2FormData', () =>
+  {
+    // prep
+    const json = { a: 1, b: 2, }
+    const form = Convertor.JSON2FormData(json)
+    // has 'a'
+    expect(
+      form.get('a')
+    ).toBe('1')
+
+    // has 'b'
+    expect(
+      form.get('b')
+    ).toBe('2')
+  })
+})
