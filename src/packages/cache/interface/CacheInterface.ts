ff1c3b0c3c88a3214a676b10926ac989639c6eb6
copydog <775150@qq.com>
2018-06-19 17:55:06 +0800

    refactor(CacheInterface.ts): update interface

--- src/packages/cache/interface/CacheInterface.ts
+++ src/packages/cache/interface/CacheInterface.ts
@@ -1,15 +1,24 @@
 import Cache from '../engine/Cache'
 import { AnyData, AnyObject } from 'rua-core/lib/Types'
 
-interface CacheInterface {
+interface CacheInterface
+{
+  length: number
+
   useStore(storeName: string): CacheInterface
+
   get(key: string, defaultValue?: any): AnyData
+
   set(key: string, value: string, time?: number): AnyData
+
   remove(key: string): AnyData
+
   clear(): AnyObject
-  length(): number
+
   keys(): string[]
+
   all(): AnyData
+
   restore(): Promise<void>
 }
 
