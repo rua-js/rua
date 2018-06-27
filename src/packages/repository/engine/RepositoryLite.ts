2528750c7cb6fb7d6c4db52578f8b096d0400b19
copydog <775150@qq.com>
2018-06-27 18:09:59 +0800

    feat(RepositoryLite.ts): add .has, .merge, .all

--- src/packages/repository/engine/RepositoryLite.ts
+++ src/packages/repository/engine/RepositoryLite.ts
@@ -1,55 +1,69 @@
-import { AnyObject, AnyData } from '../../type/data'
+import { AnyData, AnyObject } from '../../type/data'
 import { RepositoryLiteInterface } from '../interface'
-import * as _ from 'lodash'
 
 class RepositoryLite implements RepositoryLiteInterface
 {
-  protected memory: AnyObject = {}
+  protected data: AnyObject = {}
 
   public get length(): number
   {
-    return Object.keys(this.memory).length
+    return Object.keys(this.data).length
+  }
+
+  public load(data: AnyObject): AnyObject
+  {
+    return this.data = { ...data }
+  }
+
+  public merge(data: AnyObject): AnyObject
+  {
+    return this.data = { ...this.data, ...data }
   }
 
   public set(key: string, data: AnyData): AnyData
   {
-    return this.memory[key] = data
+    return this.data[key] = data
   }
 
   public get(key: string, defaultValue?: AnyData): AnyData
   {
-    return this.memory[key] || defaultValue
+    return this.data[key] || defaultValue
+  }
+
+  public has(key: string): boolean
+  {
+    return !!this.data[key]
   }
 
   public remove(key: string): AnyData
   {
-    const data = this.memory[key]
-    delete this.memory[key]
+    const data = this.data[key]
+    delete this.data[key]
 
     return data
   }
 
   public clear(): AnyObject
   {
-    const data = this.memory
-    this.memory = {}
+    const data = this.data
+    this.data = {}
 
     return data
   }
 
   public keys(): string[]
   {
-    return Object.keys(this.memory)
+    return Object.keys(this.data)
   }
 
   public values(): any[]
   {
-    return Object.values(this.memory)
+    return Object.values(this.data)
   }
 
   public all(): AnyObject
   {
-    return this.memory
+    return this.data
   }
 }
 
