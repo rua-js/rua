bc94738863d075bef8a27ed121b049a4694dc87a
copydog <775150@qq.com>
2018-09-06 17:30:50 +0800

    fix: lazy load store

--- src/packages/decorator/Action.ts
+++ src/packages/decorator/Action.ts
@@ -1,15 +1,15 @@
 export default function Action(action: string)
 {
-  // @ts-ignore
-  const store = global.reduxStore
-
   return function (target: any, key: string)
   {
     target[key] = function ()
     {
+      // @ts-ignore
+      const store = global.reduxStore
+
       if (!store)
       {
-        console.error('[Decorator]Action required global.reduxStore')
+        return console.error('[Decorator]Action required global.reduxStore')
       }
 
       store.dispatch({
