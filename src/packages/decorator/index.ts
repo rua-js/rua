7eecaeb44d128e3204d79d1b67d81617956ff8cb
copydog <775150@qq.com>
2018-09-11 10:21:45 +0800

    chore: clean up decorator

--- src/packages/decorator/index.ts
+++ src/packages/decorator/index.ts
@@ -1,28 +1,15 @@
 import Freeze from './Freeze'
 import Listen from './Listen'
 import Instance from './Instance'
-import Api from './Api'
 import AutoBind from './AutoBind'
-import Action from './Action'
-import Actions from './Actions'
 import Value from './Value'
 import HOF from './HOF'
 
-const Decorator = {
-  Listen,
-  Instance,
-  API: Api,
-  AutoBind,
-}
-
 export {
-  Decorator,
   Freeze,
   Listen,
   Instance,
   AutoBind,
-  Action,
-  Actions,
   Value,
   HOF,
 }
