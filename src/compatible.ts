16e9fd38ef954d149bfa6444d51968ac70b8a6fa
copydog <775150@qq.com>
2018-09-15 21:04:58 +0800

    refactor: drop compatible

--- src/compatible.ts
+++
@@ -1,8 +0,0 @@
-export {
-  EMPTY_ARRAY as emptyArray,
-  EMPTY_OBJECT as emptyObject,
-  emptyObjectFn as fnEmptyObject,
-  emptyArrayFn as fnEmptyArray,
-} from './packages/rua/shared'
-
-export { _ } from './packages/core/third-party'
